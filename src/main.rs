b6926901ddaf433ffbcc37d466803d153ca496c5
hongyang <1664698982@qq.com>
2024-03-17 22:08:19 +0800

    创建一个Rust工程，
    （1）添加一个一层子模块，循环打印从’a’~’Z’ 之间的所有字符
    
    （2）添加一个二层子模块，循环打印从’A’~’z’ 之间的所有字符
    
    （3）使用Cargo编译运行此工程
    
    （需要自行发现其中的细节，一个考点是：ascii码字符的顺序）

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+pub fn print_characters() {
+    for char1 in 'a'..='z' {
+        println!("{}", char1);
+        for char2 in 'A'..='z' {
+            println!("{}", char2);
+        }
+    }
+}
+
 fn main() {
-    let a: u32 = 1;
-    println!("Hello World!{}", a);
+    print_characters();
 }
