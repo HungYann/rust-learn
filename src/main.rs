e118b95e9f1f42f391575791345e605491229f9d
hongyang <1664698982@qq.com>
2024-04-13 21:02:16 +0800

    第二课：Rust的所有权作业

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-pub fn print_characters() {
-    for char1 in 'a'..='z' {
-        println!("{}", char1);
-        for char2 in 'A'..='z' {
-            println!("{}", char2);
-        }
-    }
+fn foo(s: &String) {
+    println!("in fo")
 }
 
 fn main() {
-    print_characters();
+
 }
+
